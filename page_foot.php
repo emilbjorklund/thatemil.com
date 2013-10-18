aa390310800f8ae63625e66011235c7b9106dcf6
Emil Björklund <emil.bjorklund@inuse.se>
2013-10-18 12:40:30 +0200

    Added prism loading.

--- page_foot.php
+++ page_foot.php
@@ -5,9 +5,23 @@
 			<p class="site-nav-jump"><a href="#top">Back (Top)</a></p>
 		</footer>
 	</div><!-- /.wrap -->
-	<!-- <script src="/assets/js/matchmedia.js"></script> -->
-	<!-- <script src="/assets/js/picturefill.js"></script> -->
+	
 	<script src="/assets/js/global.min.js"></script>
+	<script src="/assets/js/prism.js"></script>
+	<script>
+		var s = '/assets/css/prism.css',
+			d = document,
+			c = null;
+		if (d.createStyleSheet) {
+			try {d.createStyleSheet(s);} catch (e){};
+		} 
+		else { 
+			c=d.createElement('link');
+			c.rel='stylesheet';
+			c.href=s;
+			d.getElementsByTagName("head")[0].appendChild(c);
+		}
+	</script>
 	<?php perch_content('Analytics'); ?>
 </body>
 </html>
